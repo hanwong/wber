254e6b9ddafb29775d79c4b9e8e0b117bd90f1f0
hanwong <hanationbear@gmail.com>
2018-07-22 20:30:43 +0900

    Setting initial graphql express server

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,7 @@ import logger from 'morgan'
 class App {
   public app: GraphQLServer
   constructor () {
-    this.app = new GraphQLServer({
-    })
+    this.app = new GraphQLServer({})
     this.middlewares()
   }
   private middlewares = () : void => {
