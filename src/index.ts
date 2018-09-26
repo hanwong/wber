f867d89629a16ea1c565bda3b9f33582e4d4faa9
hanwong <hanationbear@gmail.com>
2018-09-26 11:17:06 +0900

    StartPhoneVerification

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import dotenv from 'dotenv'
-import app from './app'
-import { Options } from 'graphql-yoga'
-import { createConnection } from 'typeorm'
-import connectionOptions from './ormConfig'
+import dotenv from 'dotenv';
+import app from './app';
+import { Options } from 'graphql-yoga';
+import { createConnection } from 'typeorm';
+import connectionOptions from './ormConfig';
 
-dotenv.config()
+dotenv.config();
 
-const PORT : number | string =  process.env.PORT || 4000
-const PLAYGROUND_ENDPOINT : string = '/plaryground'
-const GRAPHGL_ENDPOINT : string = '/graphgl'
+const PORT : number | string =  process.env.PORT || 4000;
+const PLAYGROUND_ENDPOINT : string = '/plaryground';
+const GRAPHGL_ENDPOINT : string = '/graphgl';
 
 const appOptions : Options = {
   port: PORT,
   playground: PLAYGROUND_ENDPOINT,
   endpoint: GRAPHGL_ENDPOINT
-}
+};
 
-const handleAppStart = () => console.log(`Listening on port ${PORT}`)
+const handleAppStart = () => console.log(`Listening on port ${PORT}`);
 
 createConnection(connectionOptions)
   .then(() => {
---
+++ src/utils/sendSMS.ts
@@ -0,0 +1,12 @@
+import Twilio from "twilio";
+
+const twilioClient = Twilio(process.env.TWILIO_SID || 'AC6f9595b8156fbfbd2b719e48e85392e1', process.env.TWILIO_TOKEN || 'b3135069146f8ac32c52b7796494bedf');
+export const sendSMS = (to: string, body: string) => {
+  return twilioClient.messages.create({
+    body,
+    to,
+    from: process.env.TWILIO_PHONE || '+13195354117'
+  });
+};
+export const sendVerificationSMS = (to: string, key: string) =>
+  sendSMS(to, `Your verification key is: ${key}`);
