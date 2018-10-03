f5a16e40da28b54952838beb8ddd9152151ca464
hanwong <hanationbear@gmail.com>
2018-10-03 13:20:11 +0900

    modify settings

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
-import dotenv from 'dotenv';
-import app from './app';
-import { Options } from 'graphql-yoga';
-import { createConnection } from 'typeorm';
-import connectionOptions from './ormConfig';
-
+import dotenv from "dotenv";
 dotenv.config();
 
-const PORT : number | string =  process.env.PORT || 4000;
-const PLAYGROUND_ENDPOINT : string = '/playground';
-const GRAPHGL_ENDPOINT : string = '/graphgl';
+import { Options } from "graphql-yoga";
+import { createConnection } from "typeorm";
+import app from "./app";
+import connectionOptions from "./ormConfig";
 
-const appOptions : Options = {
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND_ENDPOINT: string = "/playground";
+const GRAPHQL_ENDPOINT: string = "/graphql";
+
+const appOptions: Options = {
   port: PORT,
   playground: PLAYGROUND_ENDPOINT,
-  endpoint: GRAPHGL_ENDPOINT
+  endpoint: GRAPHQL_ENDPOINT
 };
 
 const handleAppStart = () => console.log(`Listening on port ${PORT}`);
 
 createConnection(connectionOptions)
   .then(() => {
-    app.start(appOptions, handleAppStart)
+    app.start(appOptions, handleAppStart);
   })
-  .catch(error => console.log(error))
-
+  .catch(error => console.log(error));
--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -1,17 +1,15 @@
-import { ConnectionOptions } from 'typeorm'
+import { ConnectionOptions } from "typeorm";
 
-const connectionOptions : ConnectionOptions = {
-  type: 'postgres',
+const connectionOptions: ConnectionOptions = {
+  type: "postgres",
   database: 'wber',
   synchronize: true,
   logging: true,
-  entities: [
-    'entities/**/*.*'
-  ],
+  entities: ['entities/**/*.*'],
   host: process.env.DB_ENDPOINT,
   port: 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD
-}
+};
 
-export default connectionOptions
+export default connectionOptions;
